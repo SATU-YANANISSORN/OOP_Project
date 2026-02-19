3f6a42fecfa7a1752de36bda94c357843efa3385
SATU-YANANISSORN <satu20102006@gmail.com>
2026-02-19 13:22:59 +0700

    Update UpgradePanel.java

--- src/UpgradePanel.java
+++ src/UpgradePanel.java
@@ -3,12 +3,21 @@ import javax.swing.JPanel;
 
 public class UpgradePanel extends JPanel {
     
-    private JButton upgradeBaseAtk;
-    private JButton upgradeDef;
-    private JButton upgradeMaxHp;
+    private JButton upgradeBaseAtkIncreased;
+    private JButton upgradeDefIncreased;
+    private JButton upgradeMaxIncreased;
+    private JButton upgradeBaseAtkDecrease;
+    private JButton upgradeDefDecrease;
+    private JButton upgradeMaxDecrease;
+    private JButton acceptButton;
+    private JButton nexButton;
+
+    // private int UpgradeDef = 0;
+    // private int 
 
     public UpgradePanel(MainPanel main) {
 
     }
+
     
 }
