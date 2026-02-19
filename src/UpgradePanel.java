0a7bbd38f6e4cb4c65970e147bb8aa8f618f313b
SATU-YANANISSORN <satu20102006@gmail.com>
2026-02-19 12:11:50 +0700

    upgrade panel

--- src/UpgradePanel.java
+++ src/UpgradePanel.java
@@ -1,3 +1,14 @@
-public class UpgradePanel {
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class UpgradePanel extends JPanel {
+    
+    private JButton upgradeBaseAtk;
+    private JButton upgradeDef;
+    private JButton upgradeMaxHp;
+
+    public UpgradePanel(MainPanel main) {
+
+    }
     
 }
